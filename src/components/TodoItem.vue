0beb8d4eadc97f06ad53f81e5daa8f32141f1c3c
Yegor Gunko <57867800+yegorgunko@users.noreply.github.com>
2021-04-26 10:51:53 +0300

    feat: added ability to edit items

--- src/components/TodoItem.vue
+++ src/components/TodoItem.vue
@@ -1,9 +1,24 @@
 <template>
-  <p @click="markComplete" class="todo-container">
-    <span :class="{ completed: todo.completed }">{{ todo.title }}</span>
-    <button class="remove-btn" @click="$emit('delete-todo', todo.id)">
-      &times;
-    </button>
+  <p class="todo-container">
+    <input
+      v-if="editing"
+      v-model="todo.title"
+      type="text"
+      :class="{ completed: todo.completed }"
+    />
+    <span v-else @click="markComplete" :class="{ completed: todo.completed }">
+      {{ todo.title }}
+    </span>
+
+    <span class="todo-controls">
+      <button class="edit-btn" @click="editTodo">
+        {{ editing ? 'Save' : 'Edit' }}
+      </button>
+
+      <button class="remove-btn" @click="$emit('delete-todo', todo.id)">
+        &times;
+      </button>
+    </span>
   </p>
 </template>
 
@@ -12,9 +27,18 @@ export default {
   name: 'TodoItem',
   props: ['todo'],
 
+  data() {
+    return {
+      editing: false,
+    }
+  },
+
   methods: {
     markComplete() {
       this.todo.completed = !this.todo.completed
+    },
+    editTodo() {
+      this.editing = !this.editing
     },
   },
 }
@@ -23,20 +47,25 @@ export default {
 <style lang="scss" scoped>
 $font-size: 1.4rem;
 
-.todo-container {
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  font-size: $font-size;
-
-  .remove-btn {
-    background: none;
-    border: none;
-    color: #f00;
-    font-weight: bold;
-    cursor: pointer;
+.todo {
+  &-container {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
     font-size: $font-size;
-    margin-left: 0.7rem;
+
+    .remove-btn {
+      background: none;
+      border: none;
+      color: #f00;
+      font-weight: bold;
+      cursor: pointer;
+      font-size: $font-size;
+    }
+  }
+
+  &-controls {
+    margin-left: 1rem;
   }
 }
 
